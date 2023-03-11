c6504bd54c09a6461fcc07dfc1feee5592841b24
youssefjedidi <youssefjedidi2005@gmail.com>
2023-03-11 00:15:14 -0500

    Deployed Website

--- tun-app/src/pages/Tourism.js
+++ tun-app/src/pages/Tourism.js
@@ -48,7 +48,7 @@ return (
         Height={""}
         description={"a Roman archaeological site in Jendouba"}/>
         <RoundImageCard 
-        image="\Tun_App\images\Tunisia\Flag_of_Ottoman_Empire_(1517-1793).png"
+        image="\Tun_App\images\Tunisia\Feija_National_Park.jpg"
         title={"The Dougga Archaeological Site"}
         Height={""}
         description={"a well-preserved Roman city in Siliana"}/>
