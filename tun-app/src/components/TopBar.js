9f09e3147a3a104780b5f2e383150263b3be82ed
youssefjedidi <youssefjedidi2005@gmail.com>
2023-03-18 16:38:34 -0400

    Deployed Website

--- tun-app/src/components/TopBar.js
+++ tun-app/src/components/TopBar.js
@@ -1,58 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './TopBar.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function TopBar() {
-  const [activeTab, setActiveTab] = useState("");
+  const [scrolled, setScrolled] = useState(false);
+  const location = useLocation();
 
-  const handleTabClick = (tabName) => {
-    setActiveTab(tabName);
-  };
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <nav className="top-bar">
+    <nav className={scrolled ? 'top-bar scrolled' : 'top-bar'}>
       <ul>
-        <img src="/Tun_App/images/Tunisia/tnnc.png" alt="Tunisia" className="logo" />
+        <img src="/Tun_App/images/Tunisia/tnnc.png" alt="Tunisia" className="logo"></img>
         <li>
-          <Link to="/" onClick={() => handleTabClick('Home')} className={activeTab === 'Home' ? 'active' : ''}>
-            Home
-          </Link>
+          <Link to="/" className={location.pathname === "/" ? "active" : ""}>Home</Link>
         </li>
         <li>
-          <Link
-            to="/tourism"
-            onClick={() => handleTabClick('Tourism')}
-            className={activeTab === 'Tourism' ? 'active' : ''}
-          >
-            Tourism
-          </Link>
+          <Link to="/tourism" className={location.pathname === "/tourism" ? "active" : ""}>Tourism</Link>
         </li>
         <li>
-          <Link
-            to="/history"
-            onClick={() => handleTabClick('History')}
-            className={activeTab === 'History' ? 'active' : ''}
-          >
-            History
-          </Link>
+          <Link to="/history" className={location.pathname === "/history" ? "active" : ""}>History</Link>
         </li>
         <li>
-          <Link
-            to="/culture"
-            onClick={() => handleTabClick('Culture')}
-            className={activeTab === 'Culture' ? 'active' : ''}
-          >
-            Culture
-          </Link>
+          <Link to="/culture" className={location.pathname === "/culture" ? "active" : ""}>Culture</Link>
         </li>
         <li>
-          <Link
-            to="/technology"
-            onClick={() => handleTabClick('Technology')}
-            className={activeTab === 'Technology' ? 'active' : ''}
-          >
-            Technology and Research
-          </Link>
+          <Link to="/technology" className={location.pathname === "/technology" ? "active" : ""}>Technology and Research</Link>
         </li>
       </ul>
     </nav>
